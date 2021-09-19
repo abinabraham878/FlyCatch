5d25222ff3251ec3e6c29e119999bbbd10629c1c
abinabraham878 <abinabraham878@gmail.com>
2021-09-19 16:35:04 +0530

    commit

--- src/app/services/posts/posts.service.ts
+++ src/app/services/posts/posts.service.ts
@@ -2,34 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
+  private API = 'https://jsonplaceholder.typicode.com/posts/';
 
-  private API = "https://jsonplaceholder.typicode.com/posts/";
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  public getPosts(){
+  public getPosts() {
     return this.httpClient.get(this.API);
   }
 
-  public getselectedPost(id :any) {
+  public getselectedPost(id: any) {
     return this.httpClient.get(this.API + id);
   }
 
-  public comments(id: any){
-    return this.httpClient.get(this.API + id + '/comments')
+  public comments(id: any) {
+    return this.httpClient.get(this.API + id + '/comments');
   }
 
   public setpost(selectedItem: any) {
     localStorage.setItem('SelectedItem', JSON.stringify(selectedItem));
-    
   }
 
   public getpost(): any {
     const selectedItem = localStorage.getItem('SelectedItem');
-    return (selectedItem) ? JSON.parse(selectedItem) : null;
+    return selectedItem ? JSON.parse(selectedItem) : null;
   }
-
 }
